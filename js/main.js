01de8851e33ede83828488a9585776baa407675a
Koki Takahashi <hakatasiloving@gmail.com>
2014-11-11 06:18:27 +0900

    Register translate filter to Caman

--- js/main.js
+++ js/main.js
@@ -23,17 +23,36 @@ queue.loadManifest(manifest);
 
 // Extend Caman with custom colorchanging plugin
 
-Caman.Filter.register('translate', function (from, to) {
-	this.process('translate', function (color) {
+Caman.Filter.register('translate', function (fromRGB, toRGB) {
+	var from = colorConvert.rgb.hsv(fromRGB);
+	var to = colorConvert.rgb.hsv(toRGB);
 
+	this.process('translate', function (colorRGB) {
+		var color = colorConvert.rgb.hsv([colorRGB.r, colorRGB.g, colorRGB.b]);
+
+		color[0] = (color[0] + to[0] - from[0] + 360) % 360;
+		if (color[1] <= from[1]) color[1] = color[1] * to[1] / from[1];
+		else color[1] = 255 - (255 - color[1]) * (255 - to[1]) / (255 - from[1]);
+		if (color[2] <= from[2]) color[2] = color[2] * to[2] / from[2];
+		else color[2] = 255 - (255 - color[2]) * (255 - to[2]) / (255 - from[2]);
+
+		var RGB = colorConvert.hsv.rgb(color);
+		colorRGB.r = RGB[0];
+		colorRGB.g = RGB[1];
+		colorRGB.b = RGB[2];
+
+		return colorRGB;
 	});
 });
 
 function handleComplete() {
 	$(document).ready(function () {
 		$('#image-field').prepend(queue.getResult('hana.base'));
+		var timer = new Date();
 		Caman('#canvas', 'img/hana/color.png', function () {
-			this.hue(60).saturation(100).brightness(-40).exposure(10).render(function () {
+			this.translate([255, 255, 224], [255, 0, 0]);
+			this.render(function () {
+				console.log('Rendering Time: ' + (new Date() - timer));
 				$('#image-field').removeClass('invisible');
 			});
 		});
